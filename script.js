7b39918381c5d642b8dcb21de8f713d427ecb934
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-03-18 22:17:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // 9RHnWMKTbB3kwMrboNxMrmmu2Inj1R
 
 function testLink(){
-    window.location.replace("https://api.figma.com/v1/files/hb3RO2j2NG55RGChNokQJQ");
+    // window.location.replace("https://api.figma.com/v1/files/hb3RO2j2NG55RGChNokQJQ");
+    getFigmaObjTree(9RHnWMKTbB3kwMrboNxMrmmu2Inj1R, hb3RO2j2NG55RGChNokQJQ);
+}
+
+async function getFigmaObjTree(figmaApiKey, figmaId) {
+    let result = await fetch("https://api.figma.com/v1/files/" + figmaId, {
+        method: "GET",
+        headers: {
+            "X-Figma-Token": figmaApiKey
+        }
+    });
+    let figmaTreeStructure = await result.json();
+    console.log(figmaTreeStructure);
 }
