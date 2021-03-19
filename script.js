368c1261f94a71ac13a09b4522a8e8595958a415
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-03-19 16:50:12 +0200

    Added buttons

--- script.js
+++ script.js
@@ -3,21 +3,3 @@
 
 // QAaTBuBDhbwFXbzVfIIUmU
 // hb3RO2j2NG55RGChNokQJQ
-
-function testLink(){
-    // window.location.replace("https://api.figma.com/v1/files/hb3RO2j2NG55RGChNokQJQ");
-    // getFigmaObjTree(9RHnWMKTbB3kwMrboNxMrmmu2Inj1R, hb3RO2j2NG55RGChNokQJQ);
-}
-
-async function getFigmaObjTree(figmaApiKey, figmaId) {
-    let result = await fetch("https://api.figma.com/v1/files/" + figmaId, {
-        method: "GET",
-        headers: {
-            "X-Figma-Token": figmaApiKey
-        }
-    });
-    let figmaTreeStructure = await result.json();
-    console.log(figmaTreeStructure);
-}
-
-getFigmaObjTree(9RHnWMKTbB3kwMrboNxMrmmu2Inj1R, "hb3RO2j2NG55RGChNokQJQ");
