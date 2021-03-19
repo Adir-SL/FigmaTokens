5501deace17514f3c294454ffae6f8994e70a27e
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-03-19 17:32:01 +0200

    Update saveCSS.js

--- saveCSS.js
+++ saveCSS.js
@@ -1,3 +1,8 @@
 function saveStyles(){
-    
+    var a = document.createElement("a");
+    a.href="Testing"
+    a.download = "some.css"
+    document.body.appendChild(a)
+    a.click()
+    // document.body.removeChild(a)
 }
