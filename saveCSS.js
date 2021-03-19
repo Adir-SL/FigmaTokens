75a394e8bcfc8fe8ae24ec849aeb56f67c499325
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-03-19 17:45:44 +0200

    Update saveCSS.js

--- saveCSS.js
+++ saveCSS.js
@@ -1,8 +1,18 @@
 function saveStyles(){
-    var a = document.createElement("a");
-    a.href="Testing"
-    a.download = "some.css"
-    document.body.appendChild(a)
-    a.click()
-    // document.body.removeChild(a)
+    styleVars = ":root{\n"
+    var x = document.querySelectorAll("button");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        styleVars += "    --"+x[i].title.replace(/\s/g, '')+": ";
+        styleVars += x[i].style.backgroundColor+";\n";
+    }
+    styleVars += "}"
+    
+    var text = styleVars,
+    blob = new Blob([text], { type: 'text/plain' }),
+    anchor = document.createElement('a');
+    anchor.download = "variables.css";
+    anchor.href = (window.webkitURL || window.URL).createObjectURL(blob);
+    anchor.dataset.downloadurl = ['text/plain', anchor.download, anchor.href].join(':');
+    anchor.click();
 }
