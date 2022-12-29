9a02eadc05d55cde38c2d865b0001b63b5afbeb7
Adir <slutzkis@gmail.com>
2022-12-29 16:27:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,5 @@ async function getFigmaObjTree(figmaApiKey, figmaId) {
     }
 }
 getFigmaObjTree("figd_or8c8jVNaKZJQYwW-v3nwadNZtrLMXSAxtcKIv2l", "QAaTBuBDhbwFXbzVfIIUmU");
+// getFigmaObjTree("166075-78ab5635-40a7-4092-af8d-e9eb40574bbf", "QAaTBuBDhbwFXbzVfIIUmU");
 // "figd_or8c8jVNaKZJQYwW-v3nwadNZtrLMXSAxtcKIv2l"
